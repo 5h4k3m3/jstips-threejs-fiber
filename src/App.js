cae225588d205780a3097334701a699916a4d635
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-14 12:47:09 +0900

    ✨ impl spring-three

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { config, useSpring, animated } from "@react-spring/three";
 import "./App.css";
 
 const Box = (props) => {
@@ -8,18 +9,23 @@ const Box = (props) => {
   const [isHovered, setIsHovered] = useState(false);
   useFrame(() => (ref.current.rotation.x += 0.01));
 
+  const { scale } = useSpring({
+    scale: isClicked ? 2 : 1,
+    config: config.woobly,
+  });
+
   return (
-    <mesh
+    <animated.mesh
       {...props}
       ref={ref}
       onClick={() => setIsClicked(!isClicked)}
       onPointerOver={() => setIsHovered(true)}
       onPointerOut={() => setIsHovered(false)}
-      scale={isClicked ? 2 : 1}
+      scale={scale}
     >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={isHovered ? "hotpink" : "orange"} />
-    </mesh>
+    </animated.mesh>
   );
 };
 
@@ -28,13 +34,11 @@ function App() {
     <>
       <div id="canvas-container">
         <Canvas>
-          <mesh>
-            <Box position={[-1.6, 0, 0]} />
-            <Box position={[1.6, 0, 0]} />
-            <ambientLight intensity={0.5} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
-          </mesh>
+          <Box position={[-1.6, 0, 0]} />
+          <Box position={[1.6, 0, 0]} />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
         </Canvas>
       </div>
       <h1>Threejs Fiber</h1>
