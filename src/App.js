56d91d007f2a6533c44fae4cac710558544a5b04
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-14 12:41:34 +0900

    ✨ impl three-fiber

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import "./App.css";
+
+const Box = (props) => {
+  const ref = useRef();
+  const [isClicked, setIsClicked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  useFrame(() => (ref.current.rotation.x += 0.01));
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      onClick={() => setIsClicked(!isClicked)}
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+      scale={isClicked ? 2 : 1}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={isHovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <mesh>
+            <Box position={[-1.6, 0, 0]} />
+            <Box position={[1.6, 0, 0]} />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+          </mesh>
+        </Canvas>
+      </div>
+      <h1>Threejs Fiber</h1>
+      <a href="./">See more</a>
+    </>
   );
 }
 
